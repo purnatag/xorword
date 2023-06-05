647760eb5c1955c5ce3070e23b30c89bb0c5554e
purnatag <purnatag@gmail.com>
2023-06-05 16:31:34 +0530

    Plotting avg distance vs num of characters added

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,15 @@
 use rand::{distributions::Alphanumeric, Rng};
-use std::fs::File;
-use std::io::prelude::*;
+//use std::fs::File;
+//use std::io::prelude::*;
 use std::io::{self, ErrorKind};
-use std::path::Path;
+//use std::path::Path;
 
+use plotly::common::Mode;
+use plotly::{Plot, Scatter};
+
+// Function to get the binary encoding of a string and break it into 32-bit chunks
+// XOR the 32-bit chunks together to get a 32-bit encoding of the input string
+// Return as a vector of bytes
 fn xor_chunks(name: String) -> Vec<u8> {
     
     let mut name_in_binary="".to_string();
@@ -41,20 +47,24 @@ fn xor_chunks(name: String) -> Vec<u8> {
                 .zip(chunkvec.iter())
                 .for_each(|(x, y)| *x ^= *y); 
     }
-    println!("{}",sig_xor.len());
     sig_xor
 }
 
+// Generate a random alphanumeric string of the specified length
 fn gen_str_for_add(n:usize) -> String {
     let s: String = rand::thread_rng()
     .sample_iter(&Alphanumeric)
-    .take(10*n)
+    .take(n)
     .map(char::from)
     .collect();
-    println!("{}", s);
+    //println!("{}", s);
     s
 }
 
+//Calculate the hamming distance between the binary representations 
+// of two input strings
+// Return a Result instance for error handling when the input strings
+// are not of the same length
 fn calculate_distance(s1:String, s2:String) -> Result<i32, io::Error> {
     if s1.len() != s2.len() /*|| s1.len() != 32*/ {
         return Err(io::Error::new(ErrorKind::InvalidInput, "Invalid arguments to calculate distance"));
@@ -70,6 +80,7 @@ fn calculate_distance(s1:String, s2:String) -> Result<i32, io::Error> {
     Ok(dist)
 }
 
+// Convert a vector of bytes into string for easy output
 fn sign_to_string(sgn:Vec<u8>) -> String {
     let mut result:String = String::new();
     for s in sgn.iter(){
@@ -84,45 +95,74 @@ fn sign_to_string(sgn:Vec<u8>) -> String {
             str = zeros + &str;
         }
         result += &str;
-        println!("> {}", str);
+        //println!("> {}", str);
     }
     result
 }
 
+// Main function logs average distances (should increase) with letters added to the signature
 fn main(){
     let signature:String = "4:48+16:0:1440:mss*30,7:mss,sok,ts,nop,ws:df,ecn:0".to_string()
                 .chars().filter(|c| *c != ',').collect();
     //Write both the strings into a file
-    let path = Path::new("signatures_with_added_text.txt");
-    let display = path.display();
+    //let path = Path::new("signatures_with_added_text.txt");
+    //let display = path.display();
 
     // Open a file in write-only mode, returns `io::Result<File>`
-    let mut file = match File::create(&path) {
-        Err(why) => panic!("couldn't create {}: {}", display, why),
-        Ok(file) => file,
-    };
+    //let mut file = match File::create(&path) {
+    //    Err(why) => panic!("couldn't create {}: {}", display, why),
+    //    Ok(file) => file,
+    //};
     let orig_sgn: Vec<u8> = xor_chunks(signature.clone());
     let orig_str = sign_to_string(orig_sgn);
 
-    for i in 1..11 {    
-        let add_str = gen_str_for_add(i);
-        let mod_signature:String = signature.clone() + &add_str;
-        let added_sgn: Vec<u8> = xor_chunks(mod_signature);
+    //avg distance vector
+    let mut avg_dist_vector:Vec<f64> = Vec::new();
 
-        let mut entry:String = orig_str.clone() + &" ".to_string();
-        println!("\n");
-        entry = entry + & sign_to_string(added_sgn);
+    let x_values:Vec<i32> = Vec::new();
+    let base:usize = 2;
 
-        let distance = match calculate_distance(entry.chars().take(32).collect(), entry.chars().skip(33).take(32).collect()){
-            Ok(distance) => distance,
-            Err(err) => panic!("{}", err),
-        };
-        println!("Distance: {}", distance);
-        entry += & format!(" {} \n", distance);
-        // Write the signatures to `file`, returns `io::Result<()>`
-        match file.write_all(entry.as_bytes()) {
-            Err(why) => panic!("couldn't write to {}: {}", display, why),
-            Ok(_) => println!("\n successfully wrote to {}", display),
+    for i in 2..=14 {    
+        let mut avg_distance = 0.0;
+        let exp_base = base.pow(i as u32);
+        x_values.push(exp_base);
+        for _j in 0..1000 {
+            let add_str = gen_str_for_add(exp_base);
+            let mod_signature:String = signature.clone() + &add_str;
+            let added_sgn: Vec<u8> = xor_chunks(mod_signature);
+
+            let added_str:String = sign_to_string(added_sgn);
+
+            let distance = match calculate_distance(orig_str.clone(), added_str){
+                Ok(distance) => distance,
+                Err(err) => panic!("{}", err),
+            };
+            avg_distance += distance as f64;
+            if i>12 {
+                println!("Distance: {}", distance);
+            }
         }
+        avg_distance /= 1000.0;
+        println!("Average distance:{}", avg_distance);
+        avg_dist_vector.push(avg_distance);
+
+        // String to write in file
+        //let mut entry:String = orig_str.clone() + &" ".to_string();
+        //entry += & format!(" {} \n", avg_distance);
+        // Write the signatures to `file`, returns `io::Result<()>`
+        //match file.write_all(entry.as_bytes()) {
+        //    Err(why) => panic!("couldn't write to {}: {}", display, why),
+        //    Ok(_) => println!("\n successfully wrote: {}", avg_distance),
+        //}
     }
+
+    // Plot the error data
+    let trace1 = Scatter::new(x_values, avg_dist_vector)
+        .name("trace1")
+        .mode(Mode::LinesMarkers);
+    let mut plot = Plot::new();
+    plot.add_trace(trace1);
+
+    plot.show();
+    println!("{}", plot.to_inline_html(Some("avg_dist_plot")));
 }
